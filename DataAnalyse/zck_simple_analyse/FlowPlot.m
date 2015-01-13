a7aabaec620340417efeb10164ad367f1492e4b6
zck921031 <zck921031@qq.com>
2015-01-13 12:03:40 +0800

    发现一些路口流量变化比较大

--- DataAnalyse/zck_simple_analyse/FlowPlot.m
+++ DataAnalyse/zck_simple_analyse/FlowPlot.m
@@ -2,19 +2,26 @@ clear all;
 clc;
 g = ReadFlowByFile();
 
-roadid = 5;
+roadid = 94;
 timebegin = 600;
 timeend = 720;
+for r=1:155
+for i=1:7
+    disp( [r, i, sum( g(i, r, timebegin:timeend ) ) ] );
+end
+end
+
 figure(1);
 for i=1:7
      subplot(2,4,i);
      plot( reshape( g(i, roadid, timebegin:timeend ), 1, timeend-timebegin+1 ), 'color', [ 1-rem(floor(i/4),2) , 1-rem(floor(i/2),2) , 1-rem(i,2) ] );
      hold on;
 end
+hold off;
 
 figure(2);
 for i=1:7
     plot( reshape( g(i, roadid, timebegin:timeend ), 1, timeend-timebegin+1 ), 'color', [ 1-rem(floor(i/4),2) , 1-rem(floor(i/2),2) , 1-rem(i,2) ] );
     hold on;
-    disp( [i, sum( g(i, roadid, timebegin:timeend ) ) ] );
 end
+hold off;
