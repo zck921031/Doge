91f061d630fba75faafcadc47c08dfd88ffb3a27
zck921031 <zck921031@qq.com>
2015-01-23 21:24:00 +0800

    修复bug

--- Keyboard/src/decoder.java
+++ Keyboard/src/decoder.java
@@ -1,20 +1,27 @@
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
 public class decoder {
 	static public void main(String av[]){
 		String fileName = "receive.base64";
+		String outfile = "receive.rename";
 		if ( av.length > 0 ) fileName = av[0];
+		if ( av.length > 1 ) outfile = av[1];
 		File file = new File(fileName);
 		FileInputStream inputFile;
-		String rev = "";
 		try {
 			inputFile = new FileInputStream(file);
 			byte buffer[] = new byte[(int) file.length()];
 			inputFile.read(buffer);
 			inputFile.close();
-			rev = new String( (new sun.misc.BASE64Decoder() ).decodeBuffer( new String(buffer) ) );
+			byte []src = (new sun.misc.BASE64Decoder() ).decodeBuffer( new String(buffer) );
+			//System.out.println( src.length );
 			//System.out.println( "file: " + new String( buffer) );	
-			System.out.println( "file: " + rev );
+			FileOutputStream writer = new FileOutputStream( outfile );			
+			writer.write(src);
+			writer.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
